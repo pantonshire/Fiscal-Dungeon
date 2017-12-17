c792c5a890ea25bd6563eb4a4e0bd4031c05b918
Tom Panton <tom@tomandtally.co.uk>
2017-12-17 22:11:45 +0000

    Aim assist

--- core/src/com/game/entities/Player.java
+++ core/src/com/game/entities/Player.java
@@ -19,13 +19,15 @@ public class Player extends EntityLiving {
 	private static final int MAX_COINS = 100; //You die when you reach 100 coins
 	private static final int SHOOT_TIME = 24;
 	private static final double ARM_ROTATE_SPEED = Math.toRadians(8);
-	
+
 	private Animation animation;
 	private Animation bow;
 	private double armRotation;
 	private int facing;
 	private int coins;
 	private int shootTimer;
+
+	private double aimAssist = Math.toRadians(25);
 
 	public Player(World world, double x, double y) {
 		super(world, x, y, 10, 30, 2.5);
@@ -37,13 +39,13 @@ public class Player extends EntityLiving {
 						new Sequence(16, 35, 5, 4),
 						new Sequence(16, 35, 5, 4),
 						new Sequence(16, 35, 5, 4)));
-		
+
 		bow = new Animation(Textures.instance.getTexture("bow"),
 				Sequence.formatSequences(
 						new Sequence(18, 14, 0, 1),
 						new Sequence(18, 14, 4, 5).setNoLoop()));
 	}
-	
+
 	public int getCoins() {
 		return coins;
 	}
@@ -53,12 +55,12 @@ public class Player extends EntityLiving {
 		if(coins >= MAX_COINS) {
 			die();
 		}
-		
+
 		if(coins < 0) {
 			coins = 0;
 		}
 	}
-	
+
 	public void setCoins(int amount) {
 		coins = amount;
 	}
@@ -77,9 +79,37 @@ public class Player extends EntityLiving {
 			world.spawn(coin2);
 		}
 	}
-	
-	private void updateArmRotation() {
+
+	private double getTarget() {
 		double target = position.copy().add(-3, 11).angleBetween(Input.instance.getTargetPos(this, world.gameRenderer));
+		ArrayList<Enemy> enemies = world.getEnemies();
+		Enemy targetedEnemy = null;
+		double shortestDist = 0;
+		double lockOnAngle = 0;
+		for(Enemy enemy : enemies) {
+			if(enemy.isOnScreen(world.gameRenderer)) {
+				double dist = position.distBetween(enemy.getPosition());
+				if(targetedEnemy == null || dist < shortestDist) {
+					double angleToEnemy = position.angleBetween(enemy.position);
+					double delta = Math.abs(AngleHelper.angleDifferenceRadians(target, angleToEnemy));
+					if(delta <= aimAssist) {
+						shortestDist = dist;
+						targetedEnemy = enemy;
+						lockOnAngle = angleToEnemy;
+					}
+				}
+			}
+		}
+		
+		if(targetedEnemy != null) {
+			target = lockOnAngle;
+		}
+
+		return target;
+	}
+
+	private void updateArmRotation() {
+		double target = getTarget();
 		if(Math.abs(AngleHelper.angleDifferenceRadians(armRotation, target)) > 0.01) {
 			int rotationDirection = AngleHelper.getQuickestRotationDirection(armRotation, target);
 			double rotationAngle = rotationDirection * ARM_ROTATE_SPEED;
@@ -104,7 +134,7 @@ public class Player extends EntityLiving {
 					facing = (facing == 0 || facing == 2 || facing == 4) ? 0 : 1;
 				}
 			}
-			
+
 			else if(up) {
 				if(left && !right) { facing = 4; }
 				else if(right && !left) { facing = 5; }
@@ -112,19 +142,19 @@ public class Player extends EntityLiving {
 					facing = (facing == 0 || facing == 2 || facing == 4) ? 4 : 5;
 				}
 			}
-			
+
 			else {
 				if(left && !right) { facing = 2; }
 				else if(right && !left) { facing = 3; }
 			}
 		}
-		
+
 		updateArmRotation();
-		
+
 		if(shootTimer > 0) {
 			--shootTimer;
 		}
-		
+
 		if(Input.instance.isPerformingAction(Action.ATTACK) && shootTimer == 0) {
 			shootTimer = SHOOT_TIME;
 			bow.setSequence(1, true);
@@ -132,7 +162,7 @@ public class Player extends EntityLiving {
 			world.spawn(new Arrow(world, spawnPos.x, spawnPos.y, armRotation, 7));
 			SoundEffects.instance.play("schut", 1, 1, 0);
 		}
-		
+
 		ArrayList<Coin> coins = world.getCoins();
 		for(Coin coin : coins) {
 			if(hitbox.intersectsHitbox(coin.hitbox)) {
@@ -147,29 +177,29 @@ public class Player extends EntityLiving {
 			renderBody(renderer);
 			renderArm(renderer);
 		}
-		
+
 		else {
 			renderArm(renderer);
 			renderBody(renderer);
 		}
 	}
-	
+
 	private void renderBody(LayerRenderer renderer) {
 		boolean walking = isWalking();
 		if(!animation.isPaused() && !walking) { animation.resetFrame(); }
 		animation.setPaused(!walking);
 		if(!animation.isPaused()) { animation.setSequence(facing, false); }
-		
+
 		renderer.getSpriteBatch().draw(animation.getFrame(), (float)(position.x - (animation.getFrameWidth() / 2)), (float)(position.y - (animation.getFrameHeight() / 2)));
 		animation.updateTimer();
 	}
-	
+
 	private void renderArm(LayerRenderer renderer) {
 		float renderRotation = (float)Math.toDegrees(armRotation) - 90;
 		Vector armPos = position.copy().add(-3, 11);
 		renderer.getSpriteBatch().draw(bow.getFrame(), (float)armPos.x - (bow.getFrameWidth() / 2), (float)armPos.y - (bow.getFrameHeight() / 2), 9, 1, bow.getFrameWidth(), bow.getFrameHeight(), 1, 1, renderRotation);
 		bow.updateTimer();
-		
+
 		if(bow.getSequence() == 1 && bow.isDone()) {
 			bow.setSequence(0, true);
 		}
